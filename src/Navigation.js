c7bdf724cc4d653018085ad63b99699f4fc14baf
Uwaoma Cyruz <uwaomacyruz@gmail.com>
2023-04-02 22:23:43 -0700

    changed text color

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,18 +6,18 @@ function Navigation() {
   return (
     <div>
       <div className="header flex3">
-        <div class="flex-items">
-          <a href="mailto:campaigns@chainfundit.com">
+        <div className="flex-items">
+          <a href="mailto:campaigns@chainfundit.com" className="wt">
             Email:Campaigns@ChanfundIt.com
           </a>
         </div>
         <div class="flex-items">
-          <a href="tel:+442038380360" className="mytel">
+          <a href="tel:+442038380360" className="mytel wt">
             +442038380360
           </a>
         </div>
         <div class="flex-items">
-          <a href="#pay" className="mytel">
+          <a href="#pay" className="mytel wt">
             DONATE
           </a>
         </div>
